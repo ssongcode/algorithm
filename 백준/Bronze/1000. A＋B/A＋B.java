580869a044942469ad98288fad9c73aeeb93ac20
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2024-08-12 23:10:42 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static String endl = "\n";
+    static String blank = " ";
+
+    static int a, b;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
+
+    static void input() throws IOException {
+        stk();
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+    }
+
+    static void pro() throws IOException {
+        System.out.print(a + b);
+    }
+
+    static void stk() throws IOException {
+        st = new StringTokenizer(br.readLine());
+    }
+
+}
