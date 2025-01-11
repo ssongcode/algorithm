a2d3dd593abeecf6c22ef055bfa0f823434bebc3
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2025-01-11 13:35:43 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 484 ms, Memory: 121200 KB -BaekjoonHub

---
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	static int N, K;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		input();
+		pro();
+	}
+
+	static void input() throws IOException {
+		stk();
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		stk();
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+	}
+
+	static void pro() throws IOException {
+		int s = 0, e = 0, idx = 0;
+		int cnt = 0, max = 0;
+		int len = 0;
+		List<Integer> list = new ArrayList<>();
+		while (e < N) {
+			if (arr[e] % 2 != 0) {
+				if (cnt == K) {
+					for (int i = s; i < list.get(idx); i++) {
+						if (arr[i] % 2 == 0)
+							len--;
+					}
+					s = list.get(idx) + 1;
+					idx++;
+					cnt--;
+				} else {
+					list.add(e);
+					cnt++;
+					e++;
+				}
+			} else {
+				len++;
+				e++;
+			}
+
+			max = Math.max(max, len);
+		}
+
+		System.out.println(max);
+	}
+
+	static void stk() throws IOException {
+		st = new StringTokenizer(br.readLine());
+	}
+
+}
