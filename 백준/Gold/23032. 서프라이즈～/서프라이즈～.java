4db5dcfc954ba6a0b983813e6dd1c72aaa9dd4d2
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2025-01-25 22:26:44 +0900

    [Gold III] Title: 서프라이즈~, Time: 1224 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Gold/23032. 서프라이즈～/서프라이즈～.java
+++ 백준/Gold/23032. 서프라이즈～/서프라이즈～.java
@@ -1,77 +1,63 @@
 import java.io.*;
 import java.util.*;
 
-class Node{
-	int s,e,sum;
-	public Node(int s, int e, int sum) {
-		this.s = s;
-		this.e = e;
-		this.sum = sum;
-	}
-}
-
 public class Main {
-	
-  	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-//		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N+1];
-		int[] pre = new int[N+1];
-		st = new StringTokenizer(br.readLine());
-		for(int i=1;i<=N;i++) {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	static int N;
+	static int[] arr, prefix;
+
+	public static void main(String[] args) throws IOException {
+		input();
+		pro();
+	}
+
+	static void input() throws IOException {
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N + 1];
+		prefix = new int[N + 1];
+		stk();
+		for (int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			pre[i] = pre[i-1] + arr[i];
+			prefix[i] = prefix[i - 1] + arr[i];
 		}
-		List<Node> list = new ArrayList<>();
-		for(int i=1;i<=N;i++) {
-			for(int j=i;j<=N;j++) {
-				list.add(new Node(i,j,pre[j]-pre[i-1]));
-			}
-		}
-		Collections.sort(list, new Comparator<Node>() {
-			@Override
-			public int compare(Node o1, Node o2) {
-				// TODO Auto-generated method stub
-				if(o1.s==o2.s)return o1.sum-o2.sum;
-				return o1.s-o2.s;
-			}
-		});
-		int min = Integer.MAX_VALUE;
-		int ans = 0;
-		for(int i=0;i<list.size();i++) {
-			int s=0,e=list.size()-1;
-			Node node = list.get(i);
-			while(s<=e) {
-				int mid = (s+e)/2;
-				Node midNode = list.get(mid);
-				if(node.e+1>midNode.s) {
-					s=mid+1;
-				}else if(node.e+1<midNode.s) {
-					e=mid-1;
-				}else {
-					int comp = node.sum-midNode.sum;
-					int abs = Math.abs(comp);
-					int sum = node.sum+midNode.sum;
-					if(min>=abs) {
-						if(min==abs)ans=Math.max(ans, sum);
-						else ans=sum;
-						min = abs;
+	}
+
+	static void pro() throws IOException {
+		int s, e, x, y;
+		int min = Integer.MAX_VALUE, tmp, ans = 0;
+		// 임의의 그룹 선택
+		for (int i = 1; i <= N; i++) {
+			for (int j = i; j <= N; j++) {
+				x = prefix[j] - prefix[i - 1]; // x 그룹의 합
+				s = j + 1;
+				e = j + 1;
+				while (e < N + 1) {
+					y = prefix[e] - prefix[s - 1]; // y 그룹의 합
+					tmp = Math.abs(x - y);
+					if (tmp < min) {
+						min = tmp;
+						ans = x + y;
+						e++;
+					} else if (tmp == min) {
+						ans = Math.max(ans, x + y);
+						e++;
+					} else {
+						if (x > y)
+							e++;
+						else
+							break;
 					}
-					if(comp>=0) {
-						s=mid+1;
-					}else {
-						e=mid-1;
-					}
-					
 				}
 			}
 		}
 		System.out.println(ans);
-		
 	}
- 	
+
+	static void stk() throws IOException {
+		st = new StringTokenizer(br.readLine());
+	}
 
 }
