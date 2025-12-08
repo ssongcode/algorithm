4837413636b49a09af09a23b22bd644c9c03f112
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2025-12-08 16:47:15 +0900

    [Silver III] Title: 두 수의 합, Time: 316 ms, Memory: 26996 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	static int n, x;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		input();
+		pro();
+	}
+
+	static void input() throws IOException {
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		stk();
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		x = Integer.parseInt(br.readLine());
+	}
+
+	static void pro() throws IOException {
+		Arrays.sort(arr);
+		int s = 0, e = n - 1;
+		int cnt = 0;
+		while (s < e) {
+			if (arr[s] + arr[e] > x)
+				e--;
+			else if (arr[s] + arr[e] < x)
+				s++;
+			else {
+				cnt++;
+				s++;
+			}
+		}
+		System.out.println(cnt);
+	}
+
+	static void stk() throws IOException {
+		st = new StringTokenizer(br.readLine());
+	}
+
+}
