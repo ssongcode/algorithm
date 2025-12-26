58d0eb34ca10ccdb1d6462dd14b7ed42c518d6c3
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2025-12-26 10:04:40 +0900

    [Gold V] Title: 작은 벌점, Time: 176 ms, Memory: 14952 KB -BaekjoonHub

--- 백준/Gold/16498. 작은 벌점/작은 벌점.java
+++ 백준/Gold/16498. 작은 벌점/작은 벌점.java
@@ -38,26 +38,26 @@ public class Main {
 
 	static void pro() throws IOException {
 		Arrays.sort(crr);
-		// 두 개의 숫자 카드 먼저 선택
-		int s, e, mid, min, max, ans = Integer.MAX_VALUE;
+		int s, e, mid;
+		int min, max;
+		int ans = Integer.MAX_VALUE;
 		for (int i = 0; i < A; i++) {
 			for (int j = 0; j < B; j++) {
-				s = 0;
-				e = C - 1;
 				max = Math.max(arr[i], brr[j]);
 				min = Math.min(arr[i], brr[j]);
+				s = 0;
+				e = C - 1;
 				while (s <= e) {
 					mid = (s + e) / 2;
-					if (crr[mid] >= max)
-						e = mid - 1;
-					else {
-						if (crr[mid] <= min)
-							s = mid + 1;
-						else {
-							e = mid - 1;
-						}
-					}
+					
 					ans = Math.min(ans, Math.abs(Math.max(max, crr[mid]) - Math.min(min, crr[mid])));
+
+					if (crr[mid] > max)
+						e = mid - 1;
+					else if (crr[mid] < min)
+						s = mid + 1;
+					else
+						break;
 				}
 			}
 		}
