641b36be5c922016eb63d8ed08f4758da1fda33d
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2026-01-10 15:27:37 +0900

    [Silver II] Title: 영재의 시험, Time: 244 ms, Memory: 15144 KB -BaekjoonHub

--- 백준/Silver/19949. 영재의 시험/영재의 시험.java
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.java
@@ -7,8 +7,8 @@ public class Main {
 	static StringBuilder sb = new StringBuilder();
 
 	static int[] arr = new int[10];
-	static int[] ans = new int[10];
-	static int sum = 0;
+	static int[] lst = new int[10];
+	static int cnt = 0, ans = 0;
 
 	public static void main(String[] args) throws IOException {
 		input();
@@ -18,49 +18,45 @@ public class Main {
 	static void input() throws IOException {
 		stk();
 		for (int i = 0; i < 10; i++) {
-			ans[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 	}
 
 	static void pro() throws IOException {
 		recur(0);
-		System.out.println(sum);
+
+		System.out.println(ans);
 	}
 
 	static void recur(int cur) throws IOException {
 		if (!check(cur))
 			return;
+
 		if (cur == 10) {
-			sum += getScore();
+			cnt = 0;
+			for (int i = 0; i < 10; i++) {
+				if (arr[i] == lst[i])
+					cnt++;
+			}
+			if (cnt >= 5)
+				ans++;
 			return;
 		}
+
 		for (int i = 1; i <= 5; i++) {
-			arr[cur] = i;
+			lst[cur] = i;
 			recur(cur + 1);
 		}
-
 	}
 
 	static boolean check(int cur) throws IOException {
 		if (cur < 3)
 			return true;
 
-		if (arr[cur - 1] == arr[cur - 2] && arr[cur - 1] == arr[cur - 3])
+		if (lst[cur - 1] == lst[cur - 2] && lst[cur - 2] == lst[cur - 3])
 			return false;
 		else
 			return true;
-	}
-
-	static int getScore() throws IOException {
-		int score = 0;
-		for (int i = 0; i < 10; i++) {
-			if (arr[i] == ans[i])
-				score++;
-		}
-		if (score >= 5)
-			return 1;
-		else
-			return 0;
 	}
 
 	static void stk() throws IOException {
