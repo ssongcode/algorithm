4b234f0d202c59f3b29c2f1b580d3e3442ff979e
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2026-01-13 14:59:29 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 112 ms, Memory: 16372 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -6,9 +6,9 @@ public class Main {
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 
-	static int N, K;
-	static int[] arr;
-	static List<String> list = new ArrayList<String>();
+	static int n, k;
+	static int[] lst;
+	static List<String> list = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
 		input();
@@ -17,61 +17,48 @@ public class Main {
 
 	static void input() throws IOException {
 		stk();
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
 	}
 
 	static void pro() throws IOException {
-		for (int i = 1; i <= N; i++) {
-			arr = new int[i];
-			recur(0, i);
-		}
+		lst = new int[n];
+
+		recur(0, 0);
+
 		Collections.sort(list);
-		if (K > list.size())
+
+		if (list.size() < k)
 			System.out.println(-1);
 		else {
-			String str = list.get(K - 1);
-			int len = str.length();
+			String str = list.get(k - 1);
 			sb = new StringBuilder();
-			if (len == 1)
-				System.out.println(str);
-			else {
-				for (int i = 0; i < len - 1; i++) {
-					sb.append(str.charAt(i)).append("+");
-				}
-				sb.append(str.charAt(len - 1));
+			for (int i = 0; i < str.length() - 1; i++) {
+				sb.append(str.charAt(i)).append("+");
 			}
+			sb.append(str.charAt(str.length() - 1));
 			System.out.println(sb.toString());
-
 		}
 	}
 
-	static void recur(int cur, int n) throws IOException {
-		if (cur == n) {
-			if (check(arr, n)) {
+	static void recur(int cur, int sum) throws IOException {
+		if (cur > 0) {
+			if (sum == n) {
 				sb = new StringBuilder();
-				for (int i = 0; i < n; i++) {
-					sb.append(arr[i]);
+				for (int i = 0; i < cur; i++) {
+					sb.append(lst[i]);
 				}
 				list.add(sb.toString());
 			}
+		}
+		if (cur == n) {
 			return;
 		}
 
 		for (int i = 1; i <= 3; i++) {
-			arr[cur] = i;
-			recur(cur + 1, n);
+			lst[cur] = i;
+			recur(cur + 1, sum + i);
 		}
-	}
-
-	static boolean check(int[] arr, int n) throws IOException {
-		int sum = 0;
-		for (int i = 0; i < n; i++) {
-			sum += arr[i];
-		}
-		if (sum != N)
-			return false;
-		return true;
 	}
 
 	static void stk() throws IOException {
