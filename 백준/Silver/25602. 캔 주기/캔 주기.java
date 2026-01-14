e929b3950223ce35f13778888bc964e2269c608f
Song soyeon <115992523+ssongcode@users.noreply.github.com>
2026-01-14 11:22:00 +0900

    [Silver I] Title: 캔 주기, Time: 116 ms, Memory: 14816 KB -BaekjoonHub

--- 백준/Silver/25602. 캔 주기/캔 주기.java
+++ 백준/Silver/25602. 캔 주기/캔 주기.java
@@ -7,9 +7,9 @@ public class Main {
 	static StringBuilder sb = new StringBuilder();
 
 	static int N, K;
-	static int[] A, cnt;
-	static int[][] R, M, arr;
-	static int ans;
+	static int[] cnt, tmp;
+	static int[][] rang, merry, lst;
+	static int max = 0;
 
 	public static void main(String[] args) throws IOException {
 		input();
@@ -21,73 +21,74 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		A = new int[N];
+		cnt = new int[N];
 		stk();
 		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+			cnt[i] = Integer.parseInt(st.nextToken());
 		}
 
-		R = new int[K][N];
-		M = new int[K][N];
+		rang = new int[K][N];
+		merry = new int[K][N];
 		for (int i = 0; i < K; i++) {
 			stk();
 			for (int j = 0; j < N; j++) {
-				R[i][j] = Integer.parseInt(st.nextToken());
+				rang[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		for (int i = 0; i < K; i++) {
 			stk();
 			for (int j = 0; j < N; j++) {
-				M[i][j] = Integer.parseInt(st.nextToken());
+				merry[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 	}
 
 	static void pro() throws IOException {
-		arr = new int[K][2];
-		cnt = new int[N];
-		ans = 0;
+		lst = new int[K][2];
+		tmp = new int[N];
+
 		recur(0);
-		System.out.println(ans);
+
+		System.out.println(max);
 	}
 
 	static void recur(int cur) throws IOException {
 		if (!check())
 			return;
 
-		int sum, r, m;
 		if (cur == K) {
-			sum = 0;
-			for (int i = 0; i < K; i++) {
-				r = arr[i][0];
-				m = arr[i][1];
-				sum += R[i][r] + M[i][m];
-			}
-			ans = Math.max(ans, sum);
+			max = Math.max(max, getScore());
 			return;
-
 		}
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				arr[cur][0] = i;
-				arr[cur][1] = j;
-				cnt[i]++;
-				cnt[j]++;
+				lst[cur][0] = i;
+				lst[cur][1] = j;
+				tmp[i]++;
+				tmp[j]++;
 				recur(cur + 1);
-				cnt[i]--;
-				cnt[j]--;
+				tmp[i]--;
+				tmp[j]--;
 			}
 		}
 	}
 
 	static boolean check() throws IOException {
 		for (int i = 0; i < N; i++) {
-			if (cnt[i] > A[i])
+			if (tmp[i] > cnt[i])
 				return false;
 		}
-
 		return true;
+	}
+
+	static int getScore() throws IOException {
+		int sum = 0;
+		for (int i = 0; i < K; i++) {
+			sum += rang[i][lst[i][0]];
+			sum += merry[i][lst[i][1]];
+		}
+		return sum;
 	}
 
 	static void stk() throws IOException {
